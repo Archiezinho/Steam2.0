aede1b71b58dcdb73e7988dea876cd491342260f
Matheus Lopes <bellzinho1221@gmail.com>
2023-02-23 01:31:05 -0300

    Arrumando pequenos erros e comentando o código

--- js/validarCampos.js
+++ js/validarCampos.js
@@ -1,10 +1,18 @@
+/* entrada de dados:
+usuario = imput aonde será escrito o usuario;
+email = imput aonde será escrito o email;
+telefone = imput aonde será escrito o telefone;
+lblErroUsuario = paragrafo usado para o erro de ususario;
+lblErroEmail = paragrafo usado para o erro de email;
+lblErroTelefone = paragrafo usado para o erro de telefone;
+*/
 function validarCampos() {
     if (usuario.value === "" || email.value === "" || telefone.value === "" || validatorUsuario(usuario.value) !== true || validatorEmail(email.value) !== true || validatorTelefone(telefone.value) !== true) {
         if (usuario.value === "" || validatorUsuario(usuario.value) !== true) {
-            lblErrousuario.textContent = '*Você precisa preencher o seu usuario Corretamente!!';
+            lblErroUsuario.textContent = '*Você precisa preencher o seu usuario Corretamente!!';
         }
         else {
-            lblErrousuario.textContent = "";
+            lblErroUsuario.textContent = "";
         }
         if (email.value === "" || validatorEmail(email.value) !== true) {
             lblErroEmail.textContent = '*Você precisa preencher seu Email Corretamente!!';
@@ -25,14 +33,14 @@ function validarCampos() {
     }
 }
 function validatorEmail(EMAIL) {
-    let emailPattern = /^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,4})$/;
+    let emailPattern = /^([_a-zA-Z0-9-]+)*@[a-zA-Z]+(\.[a-zA-Z]+)*(\.[a-zA-Z]{2,4})$/;
     return emailPattern.test(EMAIL);
 }
 function validatorTelefone(TELEFONE) {
-    var telefonePattern = /^\(?(\d{2})\)?[ ]?(\d{5})[-]?(\d{4})$/;
+    var telefonePattern = /^\(?(\d{2})\)?[ ]?(\d{5})[- ]?(\d{4})$/;
     return telefonePattern.test(TELEFONE);
 }
 function validatorUsuario(USUARIO) {
-    var usuarioPattern = /^([a-z A-Z]{2,50})$/
+    var usuarioPattern = /^([_a-zA-Z0-9-]{2,50})$/
     return usuarioPattern.test(USUARIO);
 }
